e11593162206171cc3feae9f17faaf5cbda766e4
heckman-codes <liamheckmancode@gmail.com>
2019-12-14 11:26:25 -0800

    pulling tulus branch

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8005;
 
-var tables = [];
+var tables = [
+    {
+        name: "Tulus",
+        phone: 8058675309,
+        email: "email@domain.com",
+        id: 21,
+        time: "7:30"
+    }
+];
+
 var waitlist = [];
 
 //pseudocoding this bit
-if (tables.length > 5) {
-    waitlist.push(reservation);
-}
+// if (tables.length > 5) {
+//     waitlist.push(reservation);
+// }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,6 +34,15 @@ app.get("/tables", function (req, res) {
 
 app.get("/view", function (req, res) {
     res.sendFile(path.join(__dirname + "/view.html"))
+    return res.json(tables);
+    return res.json(waitlist);
+    // for (let i = 0; i < tables.length; i++) {
+    //     return res.json(tables[i])
+    // }
+
+    // for (let j = 0; j < waitlist.length; j++) {
+    //     return res.json()
+    // }
 })
 
 app.listen(PORT, function () {
